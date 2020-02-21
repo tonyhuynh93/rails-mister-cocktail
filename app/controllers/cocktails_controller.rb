d485a45de198e01aa4796d49c22bfb48cad17e78
tonyhuynh <tonyyyhuynhhh@gmail.com>
2020-02-21 16:28:21 +0000

    Progress cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,8 +13,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktails_path
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail) # changed
+    else
+      render :new
+    end
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.destroy_all
